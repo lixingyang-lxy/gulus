d858aa75fe3760042e4c1c01f0cc9bd3aa74b67f
lixingyang <1433565248@qq.com>
2021-03-22 16:26:06 +0800

    格式化限制

--- src/button/button-group.vue
+++ src/button/button-group.vue
@@ -1,26 +1,28 @@
 <template>
-    <div class="g-button-group">
-        <slot></slot>
-    </div>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "button-group",
-        mounted() {
-            for(let node of this.$el.children) {
-                let name = node.nodeName.toLowerCase()
-                if (name !== 'button') {
-                    console.warn(`g-button-group 的子元素应该全是 g-button，但是你写的是 ${name}`)
-                }
-            }
-        }
+export default {
+  name: "button-group",
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase();
+      if (name !== "button") {
+        console.warn(
+          `g-button-group 的子元素应该全是 g-button，但是你写的是 ${name}`
+        );
+      }
     }
+  },
+};
 </script>
 
 <style scoped>
-    .g-button-group {
-        display: flex;
-        vertical-align: middle;
-    }
+.g-button-group {
+  display: flex;
+  vertical-align: middle;
+}
 </style>
